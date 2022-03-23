a01126d6e33554e2ff5a6358decf44e2de139e77
zaac9 <72583789+zaac9@users.noreply.github.com>
2022-03-23 04:22:58 -0700

    Add files via upload

---
+++ script/about.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+// function openNav() {
+//     document.getElementById("myNav").style.display = "block";
+//   }
+  
+//   function closeNav() {
+//     document.getElementById("myNav").style.display = "none";
+//   }
