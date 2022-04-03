d8c313c14b2e57b79362adf2f3e6493ddc73ac54
zaac9 <72583789+zaac9@users.noreply.github.com>
2022-04-03 16:06:04 -0700

    Add files via upload

--- script/cursor.js
+++ script/cursor.js
@@ -12,18 +12,5 @@ const moveCursor = (e)=> {
 
 window.addEventListener('mousemove', moveCursor);
 
-// let innerCursor = document.querySelector(".inner-cursor");
-
-// document.addEventListener("mousemove", moveCursor);
-
-// function moveCursor(e) {
-//   let x = e.clientX; //gets mouse location on screen
-//   let y = e.clientY;
-//   // console.log(x, y);
-
-//   innerCursor.style.left = '${x}px';
-//   innerCursor.style.top = '${y}px';
-
-// }
 
 
