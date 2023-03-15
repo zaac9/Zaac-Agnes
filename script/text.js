3b42089a9a455a0eab0d3ec85b0d77a11b8b073e
zaac9 <72583789+zaac9@users.noreply.github.com>
2023-03-14 19:48:09 -0700

    Add files via upload

---
+++ script/text.js
@@ -0,0 +1,13 @@
+
+function changeText(text)
+{
+    var display = document.getElementById('navname');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+  function changeBack(text)
+{
+    var display = document.getElementById('navname');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
