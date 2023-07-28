6501629061cda050542c852e5efddea442cd9ed7
zaac9 <72583789+zaac9@users.noreply.github.com>
2023-07-28 14:37:35 -0700

    Add files via upload

---
+++ script/menu.js
@@ -0,0 +1,26 @@
+
+var h = document.getElementById('hamburger');
+var openMenu = document.getElementById('menuControlOpen'); // or whatever triggers the toggle
+var closeMenu = document.getElementById('menuControlClose'); // or whatever triggers the toggle
+var clickWork = document.getElementById('navWork');
+
+openMenu.addEventListener('click', function(e) {
+   h.classList.toggle('menu--active'); // or whatever your active class is
+
+//    clickWork.addEventListener('click', function(e){
+//         h.classList.toggle('menu--active');
+//     });
+});
+
+ closeMenu.addEventListener('click', function(e) {
+    h.classList.toggle('menu--active'); // or whatever your active class is
+});
+
+// FIXES ERROR OF MENU SCREEN NOT CLOSING ON HOME SCREEN WHEN CLICKING PROJECTS
+clickWork.addEventListener('click', function(e){
+    h.classList.toggle('menu--active');
+});
+
+
+
+
