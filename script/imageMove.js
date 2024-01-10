18fa248e65e3d2053224ac482d281c1f9cd2a3b0
zaac9 <72583789+zaac9@users.noreply.github.com>
2024-01-10 15:32:01 -0800

    Add files via upload

--- script/imageMove.js
+++ script/imageMove.js
@@ -1,40 +1,10 @@
-// var x_offset = 20;
-// function updatePosition( e ){
-//     // if (document.classList.contains"("))
-// 	var italist = document.getElementById("italist_img");
-//         if (italist != null){
-//         italist.style.top = e.y + "px" ;
-//         italist.style.left = e.x + "px" ;
-//         }
 
-//     var dossier = document.getElementById("dossier_img");
-//     if (dossier != null){
-//         dossier.style.top = e.y + "px" ;
-//         dossier.style.left = e.x + "px" ;
-//     }
-//     var labatie = document.getElementById("labatie_img");
-//     if (labatie != null){
-//         labatie.style.top = e.y + "px" ;
-//         labatie.style.left = e.x + "px" ;
-//     }
-//     // var summit = document.getElementById("summit_img");
-//     //     summit.style.top = e.y + "px" ;
-//     //     summit.style.left = e.x + "px" ;
-    
-//     var rain = document.getElementById("rain_img");
-//     if (rain != null){
-//         rain.style.top = e.y + "px" ;
-//         rain.style.left = e.x + "px" ;
-//     }
-// };
-
-// document.onmousemove = updatePosition;
 let aimX = 0;
 let aimY = 0;
 
 let currentX = 0;
 let currentY = 0;
-let speed = 0.09;
+let speed = 0.07;
 
 var italist = document.getElementById("italist_img");
 var dossier = document.getElementById("dossier_img");
