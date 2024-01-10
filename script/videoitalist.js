9f47cffac1634d2ce5c3a5302b00f815b59a09fd
zaac9 <72583789+zaac9@users.noreply.github.com>
2024-01-09 21:27:01 -0800

    Add files via upload

--- script/videoitalist.js
+++ script/videoitalist.js
@@ -4,7 +4,3 @@ boutique.playbackRate = 1.25;
 
 const italist = document.querySelector("#italist-full-demo")
 italist.playbackRate = 1.5;
-
-// const main_vid = document.querySelector("video-italist")
-// main_vid.playbackRate = 1.5;
-// console.log(vid.playbackRate);
